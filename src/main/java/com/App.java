2f97e2367605e6c866560b5c149e81c329f52d91
admoise <admoises.dev@gmail.com>
2024-12-18 16:33:34 -0800

    Create App.java

---
+++ src/main/java/com/App.java
@@ -0,0 +1,16 @@
+package com.insuredassurance;
+
+public class App {
+
+    private static final String MESSAGE = "Welcome to Insured Assurance Inc!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
